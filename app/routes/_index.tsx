c7dd48fc0afbcc0935d24bd70ddfed8bb1d1179e
Minesh Patel <mineshpatelis@gmail.com>
2023-06-27 00:05:52 +0100

    Update styling and copy for index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import {
-  getAllPaymentIntents,
-  getAllPayments,
-  PaymentIntentsResponse,
-} from "../models/donations.server";
+import { getAllPayments } from "../models/donations.server";
 import type { LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
@@ -47,6 +43,8 @@ export default function Index() {
   }
 
   const hourlyTimes = generateHourlyTimes();
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
 
   return (
     <main className="relative min-h-screen bg-red-600 sm:flex sm:items-center sm:justify-center">
@@ -55,9 +53,10 @@ export default function Index() {
           <h1 className="pb-4 text-center text-2xl">
             <span className="block uppercase text-red-200">RSF Totaliser</span>
           </h1>
+          <h2 className="pb-4 text-lg text-blue-300">{`Totaliser running today between 5am - 9pm, refresh page for latest totals`}</h2>
           <div className="sm:rounded-lg">
             <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-              <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+              <thead className="bg-blue-50 text-xs uppercase text-gray-700 dark:bg-blue-900 dark:text-blue-400">
                 <tr>
                   <th scope="col" className="px-6 py-3">
                     Stripe Tap to Pay Merch Total
@@ -74,17 +73,17 @@ export default function Index() {
                 </tr>
               </thead>
               <tbody>
-                <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <td className="px-6 py-4 text-lg">
+                <tr className="border-b bg-white text-white dark:border-blue-600 dark:bg-blue-700">
+                  <td className="px-6 py-4 text-2xl">
                     £{donations.totals.stripeMerch.toFixed(2)}
                   </td>
-                  <td className="px-6 py-4 text-lg">
+                  <td className="px-6 py-4 text-2xl">
                     £{donations.totals.stripeDonation.toFixed(2)}
                   </td>
-                  <td className="px-6 py-4 text-lg">
+                  <td className="px-6 py-4 text-2xl">
                     £{donations.totals.generalDonation.toFixed(2)}
                   </td>
-                  <td className="px-6 py-4 text-lg">
+                  <td className="px-6 py-4 text-2xl">
                     £{donations.totals.runningTotal.toFixed(2)}
                   </td>
                 </tr>
@@ -116,7 +115,11 @@ export default function Index() {
                   <tbody>
                     {hourlyTimes.map((time) => (
                       <tr
-                        className="border-b bg-white dark:border-gray-700"
+                        className={`border-b ${
+                          currentHour === Number(time.substring(0, 2))
+                            ? "bg-red-200 text-lg"
+                            : "bg-white"
+                        } dark:border-gray-700`}
                         key={time}
                       >
                         <td className="px-6 py-4">{time}</td>
