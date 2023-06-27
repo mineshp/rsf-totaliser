f840a1e55f23f55c5ee0805da60d959d4e1c8f32
Minesh Patel <mineshpatelis@gmail.com>
2023-06-28 00:01:35 +0100

    Update styles and copy, ready for golive

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,17 +46,40 @@ export default function Index() {
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
 
+  const lastRequested = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
   return (
-    <main className="relative min-h-screen bg-red-600 sm:flex sm:items-center sm:justify-center">
+    <main className="relative min-h-screen bg-rsfRed-400 sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <h1 className="pb-4 text-center text-2xl">
-            <span className="block uppercase text-red-200">RSF Totaliser</span>
+          <h1 className="pb-4 text-center text-4xl">
+            <span className="block uppercase text-white">RSF Totaliser</span>
           </h1>
-          <h2 className="pb-4 text-lg text-blue-300">{`Totaliser running today between 5am - 9pm, refresh page for latest totals`}</h2>
-          <div className="sm:rounded-lg">
+          <div
+            className="flex items-center bg-red-200 px-4 py-3 text-sm font-bold text-red-900"
+            role="alert"
+          >
+            <svg
+              className="mr-2 h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+            </svg>
+            <p>
+              {`Totaliser running for today between 5am - 9pm, refresh page for
+              latest totals, last update ${lastRequested}.`}
+            </p>
+          </div>
+          <h2 className="text-bold py-2 text-lg uppercase text-white">
+            Daily Total
+          </h2>
+          <div className="pt-2 sm:rounded-full">
             <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-              <thead className="bg-blue-50 text-xs uppercase text-gray-700 dark:bg-blue-900 dark:text-blue-400">
+              <thead className="bg-sky-300 text-xs uppercase text-gray-700 dark:bg-sky-300 dark:text-rsfBlue-400">
                 <tr>
                   <th scope="col" className="px-6 py-3">
                     Stripe Tap to Pay Merch Total
@@ -65,7 +88,7 @@ export default function Index() {
                     Stripe Tap to Pay Donation Total
                   </th>
                   <th scope="col" className="px-6 py-3">
-                    Donation Form (General) Total
+                    Donation Form Total
                   </th>
                   <th scope="col" className="px-6 py-3">
                     Total
@@ -73,7 +96,7 @@ export default function Index() {
                 </tr>
               </thead>
               <tbody>
-                <tr className="border-b bg-white text-white dark:border-blue-600 dark:bg-blue-700">
+                <tr className="border-b bg-white text-white dark:border-blue-600 dark:bg-rsfBlue-400">
                   <td className="px-6 py-4 text-2xl">
                     £{donations.totals.stripeMerch.toFixed(2)}
                   </td>
@@ -89,11 +112,13 @@ export default function Index() {
                 </tr>
               </tbody>
             </table>
-
-            <div className="p-4">
-              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-                  <thead className="bg-gray-50 text-xs uppercase text-red-900 dark:bg-gray-200 dark:text-red-900">
+            <h2 className="text-bold py-2 text-lg uppercase text-white">
+              Hourly breakdown
+            </h2>
+            <div className="p-2">
+              <div className="relative overflow-x-auto shadow-md">
+                <table className="w-full text-left text-sm text-rsfBlue-400 dark:text-rsfBlue-400">
+                  <thead className="bg-sky-300 text-xs uppercase text-rsfBlue-400 dark:bg-sky-300 dark:text-rsfBlue-400">
                     <tr>
                       <th scope="col" className="px-6 py-3">
                         Hourly
@@ -119,7 +144,7 @@ export default function Index() {
                           currentHour === Number(time.substring(0, 2))
                             ? "bg-red-200 text-lg"
                             : "bg-white"
-                        } dark:border-gray-700`}
+                        } dark:border-rsfBlue-400`}
                         key={time}
                       >
                         <td className="px-6 py-4">{time}</td>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rsfRed: {
+          400: "#e9483f",
+        },
+        rsfBlue: {
+          400: "#003057",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
