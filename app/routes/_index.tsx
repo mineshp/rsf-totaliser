0f99a8e21ee01e043ce675c8cf26d0ce4dd1679d
Minesh Patel <mineshpatelis@gmail.com>
2023-06-29 08:20:43 +0100

    Update hours from 5 - midnight

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -31,8 +31,8 @@ export default function Index() {
   const user = useOptionalUser();
 
   function generateHourlyTimes() {
-    var startHour = 5; // 08:00 AM
-    var endHour = 21; // 08:00 PM
+    var startHour = 5;
+    var endHour = 23;
     var times = [];
 
     for (var hour = startHour; hour <= endHour; hour++) {
@@ -71,7 +71,7 @@ export default function Index() {
             <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
           </svg>
           <p>
-            {`Totaliser running for today between 5am - 9pm, refresh page for latest totals, last update ${lastRequested}.`}
+            {`Totaliser running for today between 5am - Midnight, please refresh page for latest totals, last update ${lastRequested}.`}
           </p>
         </div>
         <h2 className="text-bold py-2 text-center text-lg uppercase text-white">
